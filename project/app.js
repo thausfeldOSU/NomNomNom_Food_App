1186d909e6d357362f4f4ccdfef636450385b25f
thausfeldOSU <hausfelt@oregonstate.edu>
2026-02-10 19:14:26 -0800

    Modified to fit our project by adding new .get() to handle our multiple pages

--- project/app.js
+++ project/app.js
@@ -32,20 +32,77 @@ app.get('/', async function (req, res) {
     }
 });
 
-app.get('/bsg-people', async function (req, res) {
+app.get('/nnn_food_items', async function (req, res) {
     try {
         // Create and execute our queries
-        // In query1, we use a JOIN clause to display the names of the homeworlds
-        const query1 = `SELECT bsg_people.id, bsg_people.fname, bsg_people.lname, \
-            bsg_planets.name AS 'homeworld', bsg_people.age FROM bsg_people \
-            LEFT JOIN bsg_planets ON bsg_people.homeworld = bsg_planets.id;`;
-        const query2 = 'SELECT * FROM bsg_planets;';
-        const [people] = await db.query(query1);
-        const [homeworlds] = await db.query(query2);
+        const query1 = `SELECT * FROM FOOD_ITEMS;`;
+        const [food] = await db.query(query1);
 
-        // Render the bsg-people.hbs file, and also send the renderer
-        //  an object that contains our bsg_people and bsg_homeworld information
-        res.render('bsg-people', { people: people, homeworlds: homeworlds });
+        // Render the nnn_food_items.hbs file, and also send the renderer
+        //  an object that contains the results of the query
+        res.render('nnn_food_items', { food: food});
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.get('/nnn_restaurants', async function (req, res) {
+    try {
+        // Create and execute our queries
+        const query1 = `SELECT * FROM RESTAURANTS;`;
+        const [restaurant] = await db.query(query1);
+
+        // Render the nnn_restaurants.hbs file, and also send the renderer
+        //  an object that contains the results of the query
+        res.render('nnn_restaurants', { restaurant: restaurant});
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.get('/nnn_customers', async function (req, res) {
+    try {
+        // Create and execute our queries
+        const query1 = `SELECT * FROM CUSTOMERS;`;
+        const [customer] = await db.query(query1);
+
+        // Render the nnn_customers.hbs file, and also send the renderer
+        //  an object that contains the results of the query
+        res.render('nnn_customers', { customer: customer});
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.get('/nnn_orders', async function (req, res) {
+    try {
+        // Create and execute our queries
+
+        /* 
+           Tommy comments 2/10
+           First off, I think turning the page for ORDERS into a view of our intersection table is appropriate. We could showcase the M:M CRUD
+           operations on this page. If you agree, then we would need more than just this SELECT statement. I am assuming it would go here
+           in this .get() block, but maybe that's for a seperate .get() block? Like would we have one .get() to catch a click on a DELETE
+           button on this page or would we keep it in here? Same question for CREATE and UPDATE.
+        */
+        const query1 = `SELECT * FROM FOOD_ITEMS_ORDERS;`;
+        const [customer] = await db.query(query1);
+
+        // Render the nnn_food_items_orders.hbs file, and also send the renderer
+        //  an object that contains the results of the query
+        res.render('nnn_food_items_orders', { customer: customer});
     } catch (error) {
         console.error('Error executing queries:', error);
         // Send a generic error message to the browser
