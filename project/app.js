0df9055820994039e05fc497f6109ba86329a369
Thomas Hausfeld <hausfelt@oregonstate.edu>
2026-03-05 20:01:50 -0800

    Removed mention of 'nnn_' in any file name or within the code per student review recommendation

--- project/app.js
+++ project/app.js
@@ -45,7 +45,7 @@ app.post('/delete_food_item', async function (req, res) {
         const foodId = req.body.delete_food_id;
 
         await db.query('CALL DELETE_FOOD_ITEM(?)', [foodId]);
-        res.redirect('/nnn_food_items');
+        res.redirect('/food_items');
     } catch (error) {
         console.error("Error deleting food item:", error);
         res.status(500).send("An error occurred while deleting the food item.");
@@ -58,7 +58,7 @@ app.post('/delete_order_food', async function (req, res) {
         const foodId = req.body.delete_food_id;
 
         await db.query('CALL DELETE_ORDER_FOOD_ITEM(?, ?)', [orderId, foodId]);
-        res.redirect('/nnn_food_items_orders');
+        res.redirect('/food_items_orders');
     } catch (error) {
         console.error("Error deleting:", error);
         res.status(500).send("An error occurred while deleting.");
@@ -76,21 +76,21 @@ app.get('/', async function (req, res) {
     }
 });
 
-app.get('/nnn_food_items', async function (req, res) {
+app.get('/food_items', async function (req, res) {
     try {
         // Create and execute our queries
-        const query1 = fs.readFileSync(path.join(__dirname, 'queries', 'nnn_food_items.sql'), 'utf8');
+        const query1 = fs.readFileSync(path.join(__dirname, 'queries', 'food_items.sql'), 'utf8');
         const [food] = await db.query(query1);
 
-        const query2 = fs.readFileSync(path.join(__dirname, 'queries', 'nnn_food_items-restaurant_list.sql'), 'utf8');
+        const query2 = fs.readFileSync(path.join(__dirname, 'queries', 'food_items-restaurant_list.sql'), 'utf8');
         const [restaurant] = await db.query(query2);
 
-        const query3 = fs.readFileSync(path.join(__dirname, 'queries', 'nnn_food_items-list.sql'), 'utf8');
+        const query3 = fs.readFileSync(path.join(__dirname, 'queries', 'food_items-list.sql'), 'utf8');
         const [foods] = await db.query(query3);
 
-        // Render the nnn_food_items.hbs file, and also send the renderer
+        // Render the food_items.hbs file, and also send the renderer
         //  an object that contains the results of the query
-        res.render('nnn_food_items', {food: food, restaurant: restaurant, foods: foods});
+        res.render('food_items', {food: food, restaurant: restaurant, foods: foods});
     } catch (error) {
         console.error('Error executing queries:', error);
         // Send a generic error message to the browser
@@ -100,15 +100,15 @@ app.get('/nnn_food_items', async function (req, res) {
     }
 });
 
-app.get('/nnn_restaurants', async function (req, res) {
+app.get('/restaurants', async function (req, res) {
     try {
         // Create and execute our queries
-        const query1 = fs.readFileSync(path.join(__dirname, 'queries', 'nnn_restaurants.sql'), 'utf8');
+        const query1 = fs.readFileSync(path.join(__dirname, 'queries', 'restaurants.sql'), 'utf8');
         const [restaurant] = await db.query(query1);
 
-        // Render the nnn_restaurants.hbs file, and also send the renderer
+        // Render the restaurants.hbs file, and also send the renderer
         //  an object that contains the results of the query
-        res.render('nnn_restaurants', { restaurant: restaurant});
+        res.render('restaurants', { restaurant: restaurant});
     } catch (error) {
         console.error('Error executing queries:', error);
         // Send a generic error message to the browser
@@ -118,15 +118,15 @@ app.get('/nnn_restaurants', async function (req, res) {
     }
 });
 
-app.get('/nnn_customers', async function (req, res) {
+app.get('/customers', async function (req, res) {
     try {
         // Create and execute our queries
-        const query1 = fs.readFileSync(path.join(__dirname, 'queries', 'nnn_customers.sql'), 'utf8');
+        const query1 = fs.readFileSync(path.join(__dirname, 'queries', 'customers.sql'), 'utf8');
         const [customer] = await db.query(query1);
 
-        // Render the nnn_customers.hbs file, and also send the renderer
+        // Render the customers.hbs file, and also send the renderer
         //  an object that contains the results of the query
-        res.render('nnn_customers', { customer: customer});
+        res.render('customers', { customer: customer});
     } catch (error) {
         console.error('Error executing queries:', error);
         // Send a generic error message to the browser
@@ -136,7 +136,7 @@ app.get('/nnn_customers', async function (req, res) {
     }
 });
 
-app.get('/nnn_food_items_orders', async function (req, res) {
+app.get('/food_items_orders', async function (req, res) {
     try {
         // Create and execute our queries
 
@@ -147,15 +147,15 @@ app.get('/nnn_food_items_orders', async function (req, res) {
            in this .get() block, but maybe that's for a seperate .get() block? Like would we have one .get() to catch a click on a DELETE
            button on this page or would we keep it in here? Same question for CREATE and UPDATE.
         */
-        const query1 = fs.readFileSync(path.join(__dirname, 'queries', 'nnn_food_items_orders.sql'), 'utf8');
+        const query1 = fs.readFileSync(path.join(__dirname, 'queries', 'food_items_orders.sql'), 'utf8');
         const [order] = await db.query(query1);
 
-        const query2 = fs.readFileSync(path.join(__dirname, 'queries', 'nnn_orders.sql'), 'utf8');
+        const query2 = fs.readFileSync(path.join(__dirname, 'queries', 'orders.sql'), 'utf8');
         const [orders] = await db.query(query2);
 
-        // Render the nnn_food_items_orders.hbs file, and also send the renderer
+        // Render the food_items_orders.hbs file, and also send the renderer
         //  an object that contains the results of the query
-        res.render('nnn_food_items_orders', { order: order, orders: orders});
+        res.render('food_items_orders', { order: order, orders: orders});
     } catch (error) {
         console.error('Error executing queries:', error);
         // Send a generic error message to the browser
